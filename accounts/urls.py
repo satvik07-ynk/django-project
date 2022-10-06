51cda1d86ff7ff59ba4625745be904f9a2124a96
Satvik Garg <54634814+satvik07-ynk@users.noreply.github.com>
2022-10-06 12:01:58 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("Equipments/",views.Equipments,name="Equipments"),
-    path("Measurements/",views.Measurements, name="Measurements"),
+    path("Measurements/",views.test, name="test"),
     # path("Basics/",views.Basics,name = "Basics"),
     # path("basic/Basics/",views.Basics,name = "Basics"),
-    path("test/",views.test,name="test"),
+    #path("test/",views.test,name="test"),
 
     ]
